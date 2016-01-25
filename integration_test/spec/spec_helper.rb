dea45364643200e0e5d3724fca89afa4d1650bcb
akry <a-yokokawa@axsh.net>
2016-01-25 17:52:07 +0900

    Add integration test executor

---
+++ integration_test/spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require 'builder'
+require 'pry'
+
+Dir['../../../spec/helpers/*.rb'].map {|f| 
+p f
+require f }
+
+RSpec.configure do |config|
+  config.expose_dsl_globally = true
+
+  config.color = true
+  config.formatter = :documentation
+
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+end
---
+++ integration_test/spec/super_simple_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'spec_helper'
+
+describe "super_simple" do
+
+  before(:all) do
+  end
+
+  it "creates one dcmgr node" do
+  end
+end
