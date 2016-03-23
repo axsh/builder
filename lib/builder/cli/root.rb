499bd012a66875c5247db6cc23125833a053541c
akry <a-yokokawa@axsh.net>
2016-03-23 17:47:44 +0900

    Refactor cli root

--- lib/builder/cli/root.rb
+++ lib/builder/cli/root.rb
@@ -4,12 +4,6 @@ module Builder::Cli
   class Root < Thor
     include Builder::Helpers::Logger
 
-    def initialize(*args)
-      super(*args)
-      Builder.recipe = YAML.load_file("builder.yml").symbolize_keys
-      Builder.config = config_loader(YAML.load_file(".builder").symbolize_keys)
-    end
-
     desc "init", "init"
     def init
       [".builder", "builder.yml"].each do |file|
@@ -17,30 +11,52 @@ module Builder::Cli
       end
     end
 
-    desc "exec", "exec"
-    def exec(name = :all)
-      Builder::Networks.provision
-      Builder::Nodes.provision(name)
-      Builder::Networks.mesh_network
+    desc "up", "up"
+    def up
+      load_db_config
+
+      list_to_provision.each do |n|
+        info "+" * 20
+        info "#{n}"
+        info "+" * 20
+        Builder::Hypervisors.const_get(db[:nodes][n][:provision][:spec][:type].capitalize).provision(n)
+      end
+
+      post_phase(db)
+    end
+
+    desc "exec_post_phase", "exec_post_phase"
+    def exec_post_phase
+      load_db_config
+      post_phase(db)
     end
 
     no_tasks {
-      def validate
-        Builder.recipe[:validated] = true
+      def load_db_config
+        Builder.db ||= YAML.load_file("builder.yml").symbolize_keys
+        Builder.config ||= YAML.load_file(".builder").symbolize_keys
       end
 
-      def config_loader(c)
-        c[:builder_root] ||= "#{File.expand_path("../../../../default_builder_dir", __FILE__)}"
-        c[:seed_image_path] ||= "#{c[:builder_root]}/seed"
-        c[:aws_access_key] = c[:aws_access_key] || ENV["AWS_ACCESS_KEY"]
-        c[:aws_secret_key] = c[:aws_secret_key] || ENV["AWS_SECRET_KEY"]
-
-        if not Dir.exist?(c[:builder_root])
-          FileUtils.mkdir_p(c[:builder_root])
-          info "builder_root directory created : #{c[:builder_root]}"
+      def list_to_provision
+        db[:nodes].inject([]) do |nodes_to_provision, (key, value)|
+          if value.include?(:provision) && (!value[:provision].include?(:provisioned) || value[:provision][:provisioned] == false)
+            nodes_to_provision << key
+          end
+          nodes_to_provision
         end
+      end
 
-        c
+      def post_phase(db)
+        if db.key?(:post_phase)
+          case db[:post_phase][:type]
+          when 'executable'
+            `./#{db[:post_phase][:file]}`
+          when 'eval'
+            eval(File.read(db[:post_phase][:file]))
+          else
+            error "type not supported"
+          end
+        end
       end
     }
   end
